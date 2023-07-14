fb4fe460339733d27cb72f6bc835fdc413aeee50
adey-px <aosteph@gmail.com>
2023-07-14 17:18:39 -0500

    Updated recipe details

--- src/app/recipes-main/recipes.model.ts
+++ src/app/recipes-main/recipes.model.ts
@@ -1,14 +1,18 @@
-//
+import { Ingredient } from "../common/ingredient.module";
+
 // Define structure for a recipe
 export class Recipe {
   public name: string
   public description: string
   public imagePath: string
+  public ingredients: Ingredient[]
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string,
+    ingredients: Ingredient[]) {
     this.name = name
     this.description = desc
     this.imagePath = imagePath
+    this.ingredients = ingredients
   }
 }
 
--- src/app/recipes-main/recipes.service.ts
+++ src/app/recipes-main/recipes.service.ts
@@ -1,14 +1,29 @@
 import {EventEmitter} from '@angular/core';
 import { Recipe } from "./recipes.model";
+import { Ingredient } from '../common/ingredient.module';
 
 // Recipe service from recipe-list
 export class RecipeService {
   singleRecipe = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-    new Recipe('Recipe One', 'Jollof rice super recipe', 'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg' ),
+    new Recipe('Recipe One',
+      'Jollof rice super recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg',
+      [
+        new Ingredient('Meat Balls', 20),
+        new Ingredient('French Files', 80)
+      ],
+    ),
 
-    new Recipe('Recipe Two', 'Yam porridge recipe', 'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg' )
+    new Recipe('Recipe Two',
+      'Yam porridge recipe',
+      'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg',
+      [
+        new Ingredient('Tomato Puree', 50),
+        new Ingredient('Zoology Carrot', 90)
+      ],
+    )
   ];
 
   getRecipes() {
